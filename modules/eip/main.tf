77f4038e0a9e8a15b1919233d418207800914632
Yellow-carpet <66062470+Yellow-carpet@users.noreply.github.com>
2022-01-10 16:18:12 +0100

    Add files via upload

---
+++ modules/eip/main.tf
@@ -0,0 +1,11 @@
+resource "aws_eip_association" "eip_assoc" {
+  instance_id   = var.instance_id
+  allocation_id = aws_eip.eip.id
+}
+
+resource "aws_eip" "eip" {
+  vpc = true
+  tags = {
+   Name = "sofiane_eip"
+    }
+}
